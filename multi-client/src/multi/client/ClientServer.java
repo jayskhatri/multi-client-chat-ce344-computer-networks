5b44aa052501263b49538559ee58b47a68700b72
ishakhimsurya <ishakhimsurya29@gmail.com>
2019-08-26 22:34:02 +0530

    added clientchat and clientserver

---
+++ multi-client/src/multi/client/ClientServer.java
@@ -0,0 +1,26 @@
+package multi.client;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Isha Khimsurya
+ */
+public class ClientServer {
+    public static void main(String [] args){
+		
+		Object[] selectioValues = { "Server","Client"};
+		String initialSection = "Server";
+		
+		Object selection = JOptionPane.showInputDialog(null, "Login as : ", "MyChatApp", JOptionPane.QUESTION_MESSAGE, null, selectioValues, initialSection);
+		if(selection.equals("Server")){
+                   String[] arguments = new String[] {};
+			new ChatServerSync().main(arguments);
+		}else if(selection.equals("Client")){
+			String IPServer = JOptionPane.showInputDialog("Enter the Server ip adress");
+                        String[] arguments = new String[] {IPServer};
+			new ClientChat().main(arguments);
+		}
+		
+	}
+}
